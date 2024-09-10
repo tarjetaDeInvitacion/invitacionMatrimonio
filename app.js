627adbff5d9167c55ffd62ee8486bb673e354bd4
andresPalmab <palmabustosandres@gmail.com>
2024-09-10 13:41:26 -0500

    proyecto terminado

---
+++ app.js
@@ -0,0 +1,23 @@
+const currentYear = new Date().getFullYear();
+const countDownDate = new Date(`November 2, ${currentYear} 00:00:00`).getTime();
+
+const countDownFuntion = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").textContent = days;
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("seconds").textContent = seconds;
+
+    if (distance < 0) {
+        clearInterval(countdownFunction);
+        document.querySelector('.countdown').innerHTML = "¡Cuenta regresiva completada!";
+    }
+}, 1000);
+
